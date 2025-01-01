b4b9de8db2d2f0e9edb66490d5682061d9ee8609
Vignesh N <vigneshrage@gmail.com>
2025-01-01 14:17:51 +0530

    connecting to mongodb, toastify, saving to database

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,6 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const Generate = () => {
+    const [links, setLinks] = useState([{ link: "", linkText: "" }]);
+    const [handle, sethandle] = useState("");
+    const [pic, setpic] = useState("");
+
+    const handleChange = (index, link, linktext) => {
+        setLinks((initialLinks) => {
+            return initialLinks.map((item, i) => {
+                if (i == index) {
+                    return { link, linktext };
+                } else {
+                    return item;
+                }
+            });
+        });
+    };
+
+    const addLink = () => {
+        setLinks(links.concat([{ link: "", linkText: "" }]));
+    };
+
+    const submitLinks = async (text, link, handle) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            links: links,
+            handle: handle,
+            pic: pic,
+        });
+
+        console.log(raw);
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow",
+        };
+
+        const r = await fetch("http://localhost:3000/api/add", requestOptions);
+        const result = await r.json();
+        toast(result.message);
+    };
+
     return (
         <>
             <div className="bg-green-400 min-h-screen grid grid-cols-2 mt-16 text-gray-800">
@@ -9,30 +55,91 @@ const Generate = () => {
                     <div className="flex flex-col gap-5">
                         <h2 className="font-semibold text-xl">Step 1: Claim your Handle</h2>
                         <div className="mx-4">
-                        <input className="px-4 py-1 rounded-md focus:outline-green-300" type="text" placeholder="branchify/yourname" />
-
+                            <input
+                                value={handle || ""}
+                                onChange={(e) => {
+                                    sethandle(e.target.value);
+                                }}
+                                className="px-4 py-1 rounded-md focus:outline-green-300"
+                                type="text"
+                                placeholder="branchify/yourname"
+                            />
                         </div>
                         <h2 className="font-semibold text-xl">Step 2: Add your Links</h2>
-                        <div className="mx-4 flex gap-4">
+                        {links &&
+                            links.map((item, index) => {
+                                return (
+                                    <div key={index} className="mx-4 flex gap-4">
+                                        <input
+                                            value={item.linktext || ""}
+                                            onChange={(e) => {
+                                                handleChange(index, item.link, e.target.value);
+                                            }}
+                                            className="px-4 py-1 rounded-md focus:outline-green-300"
+                                            type="text"
+                                            placeholder="Enter Title"
+                                        />
 
-                        <input className="px-4 py-1 rounded-md focus:outline-green-300" type="text" placeholder="Enter Title" />
-                      
-                        <input className="px-4 py-1 rounded-md focus:outline-green-300" type="text" placeholder="Enter URL" />
-                        <button className="bg-[rgba(233,192,233,255)] px-4 py-1 rounded-md font-semibold border border-black">Add</button>
-                        </div>
-                        <h2 className="font-semibold text-xl">Step 3: Personalize your Branch</h2>
+                                        <input
+                                            value={item.link || ""}
+                                            onChange={(e) => {
+                                                handleChange(index, e.target.value, item.linktext);
+                                            }}
+                                            className="px-4 py-1 rounded-md focus:outline-green-300"
+                                            type="text"
+                                            placeholder="Enter URL"
+                                        />
+                                    </div>
+                                );
+                            })}
+                        <button
+                            onClick={() => addLink()}
+                            className="bg-[rgba(233,192,233,255)] px-4 py-1 rounded-md font-semibold border border-black w-fit mx-4"
+                        >
+                            + Add Link
+                        </button>
+                        <h2 className="font-semibold text-xl">
+                            Step 3: Personalize your Branch
+                        </h2>
                         <div className="mx-4 flex flex-col gap-4 ">
-                        <input className="px-4 py-1 rounded-md focus:outline-green-300 w-fit" type="text" placeholder="Link your picture" />
- 
-                        <button className="bg-[rgba(233,192,233,255)] px-4 py-1 rounded-md font-semibold w-fit my-5 border border-black">Create Branch</button>
+                            <input
+                                value={pic || ""}
+                                onChange={(e) => {
+                                    setpic(e.target.value);
+                                }}
+                                className="px-4 py-1 rounded-md focus:outline-green-300 w-fit"
+                                type="text"
+                                placeholder="Link your picture"
+                            />
+
+                            <button
+                                onClick={() => {
+                                    submitLinks();
+                                }}
+                                className="bg-[rgba(233,192,233,255)] px-4 py-1 rounded-md font-semibold w-fit my-5 border border-black"
+                            >
+                                Create Branch
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div className="col2 bg-[rgba(233,192,233,255)]  w-full h-screen flex justify-center items-center">
+                <div className="col2 bg-[rgba(233,192,233,255)]  w-full h-full flex justify-center ">
                     <img
-                        className="h-full object-contain"
+                        className="h-screen object-contain"
                         src="/generate.png"
                         alt="generate your links"
+                    />
+                    <ToastContainer
+                        position="top-center"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick={false}
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="dark"
                     />
                 </div>
             </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-
     return (
         <nav className="logo bg-white w-[80vw] left-[10vw]  p-3 rounded-xl sticky top-8 flex justify-between items-center ">
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <div className="logo flex items-center">
-                    <Image src="/Branchify.png" width={50} height={50} alt='Logo' />
-                    <h1 className='font-bold text-lg'>Branchify</h1>
+                    <Link href="/">
+                        <Image src="/Branchify.png" width={50} height={50} alt="Logo" />
+                    </Link>
+                    <Link href="/">
+                        <h1 className="font-bold text-lg">Branchify</h1>
+                    </Link>
                 </div>
-                <div className=''>
-                    <ul className='flex items-center  gap-4'>
+                <div className="">
+                    <ul className="flex items-center  gap-4">
                         <li>Templates</li>
                         <li>Discover</li>
                         <li>Pricing</li>
@@ -19,12 +23,16 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-            <div className='flex gap-5 items-center'>
-                <button className='font-semibold border-2 border-black rounded p-2 text-sm'>Login</button>
-                <button className='font-semibold border-2 border-black rounded p-2 text-sm'>Sign Up</button>
+            <div className="flex gap-5 items-center">
+                <button className="font-semibold border-2 border-black rounded p-2 text-sm">
+                    Login
+                </button>
+                <button className="font-semibold border-2 border-black rounded p-2 text-sm">
+                    Sign Up
+                </button>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
